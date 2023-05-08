221aca823b8e29fb60e1c3fabb0e8003f9b372e8
nakajimadaniel <danieln17.contato@outlook.com>
2023-05-08 17:10:10 -0300

    feat: findAll paginated

--- src/main/java/com/example/spring_training_person/PersonController.java
+++ src/main/java/com/example/spring_training_person/PersonController.java
@@ -3,6 +3,13 @@ package com.example.spring_training_person;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -37,8 +45,14 @@ public class PersonController {
 
   })
 
-  public List<PersonVO> findAll() {
-    return service.findAll();
+  public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
+      @RequestParam(value = "page", defaultValue = "0") Integer page,
+      @RequestParam(value = "limit", defaultValue = "12") Integer limit,
+      @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+    var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+    Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
+    return ResponseEntity.ok(service.findAll(pageable));
   }
 
   @CrossOrigin(origins = "http://localhost:8080")
--- src/main/java/com/example/spring_training_person/PersonServices.java
+++ src/main/java/com/example/spring_training_person/PersonServices.java
@@ -3,6 +3,12 @@ package com.example.spring_training_person;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
@@ -14,11 +20,22 @@ public class PersonServices {
 
   @Autowired
   PersonMapper mapper;
+  @Autowired
+  PagedResourcesAssembler<PersonVO> assembler;
 
-  public List<PersonVO> findAll() {
-    var persons = DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
-    persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-    return persons;
+  public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
+
+    var personPage = repository.findAll(pageable);
+
+    var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+
+    personVosPage
+        .map(p -> p.add(linkTo(methodOn(PersonController.class).findById(((PersonVO) p).getKey())).withSelfRel()));
+
+    Link link = linkTo(
+        methodOn(PersonController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc"))
+        .withSelfRel();
+    return assembler.toModel(personVosPage, link);
   }
 
   public PersonVO findById(Long id) {
